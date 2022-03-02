08d8eac277862634d0468d6873cdee35e3045b0f
EGit <67373356+Ex00Git@users.noreply.github.com>
2022-03-02 11:27:09 +0100

    Côté Sérial

---
+++ serialclient.py
@@ -0,0 +1,35 @@
+import serial
+import pyautogui
+# COM CONNECTION
+
+ser = serial.Serial(
+  port='COM3',\
+  baudrate=115200,\
+  parity=serial.PARITY_NONE,\
+  stopbits=serial.STOPBITS_ONE,\
+  bytesize=serial.EIGHTBITS,\
+    timeout=1000
+)
+
+# COM DETECTION & MOUSE
+
+while True:
+  result = str(ser.readline())
+  up = str('U')
+  down = str('D')
+  left = str('L')
+  right = str('R')
+  click = str('C')
+
+  if up in result:
+    pyautogui.moveRel(0,-50, duration = 0.1)
+  if down in result:
+    pyautogui.moveRel(0,50, duration = 0.1)
+  if left in result:
+    pyautogui.moveRel(-50,0, duration = 0.1)
+  if right in result:
+    pyautogui.moveRel(50,0, duration = 0.1)
+  if click in result:
+      pyautogui.click(pyautogui.position())
+
+
