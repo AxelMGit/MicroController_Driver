c1419be9c84588e8b53d5cc99d8347490830307b
EGit <67373356+Ex00Git@users.noreply.github.com>
2022-03-09 10:53:31 +0100

    Optimisation du côté tx + Del éléments inutiles

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
-lcd_text = ""
 tinkercademy.crash_sensor_setup(DigitalPin.P13)
 pins.digital_read_pin(DigitalPin.P0)
-I2C_LCD1602.show_string(lcd_text, 0, 0)
-I2C_LCD1602.lcd_init(39)
 
 def on_forever():
-    global lcd_text
-
     if pins.analog_read_pin(AnalogPin.P1) >= 1020:
-        #UP
-        serial.write_line("U")
+        # UP
+        serial.write_line("U1")
     if pins.analog_read_pin(AnalogPin.P1) <= 10:
-        #DOWN
-        serial.write_line("D")
-        
-    if pins.analog_read_pin(AnalogPin.P2) >= 1020:
-        #LEFT
-        serial.write_line("L")
+        # DOWN
+        serial.write_line("D1")
     if pins.analog_read_pin(AnalogPin.P2) <= 10:
-        #RIGHT
-        serial.write_line("R")
+        # LEFT
+        serial.write_line("L1")
+    if pins.analog_read_pin(AnalogPin.P2) >= 1020:
+        # RIGHT
+        serial.write_line("R1")
     if tinkercademy.crash_sensor():
-        #DETECTION CRASH
-        serial.write_line("CS")
-
-    if input.button_is_pressed(Button.A):
-        #BUZZER
-        pins.digital_write_pin(DigitalPin.P14, 1)
-    else:
-        #BUZZER
-        pins.digital_write_pin(DigitalPin.P14, 0)
+        # DETECTION
+        serial.write_line("CS1")
 
 basic.forever(on_forever)
